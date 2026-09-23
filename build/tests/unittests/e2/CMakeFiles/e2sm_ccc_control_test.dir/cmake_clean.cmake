2541cec16a726e3b48fcc3204bc9aec35a45bbfb
fypuom22 <fypuom22@gmail.com>
2026-09-23 16:20:30 +0530

    delay , connect 3 UEs for 3 slices

---
+++ build/tests/unittests/e2/CMakeFiles/e2sm_ccc_control_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/e2sm_ccc_control_test.dir/e2sm_ccc_control_test.cpp.o"
+  "CMakeFiles/e2sm_ccc_control_test.dir/e2sm_ccc_control_test.cpp.o.d"
+  "e2sm_ccc_control_test"
+  "e2sm_ccc_control_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/e2sm_ccc_control_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
